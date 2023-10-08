b370e0e1cc0dddeeb66c410fae9a936c98774522
nikpaiyudev <nikhilradhakrishnan.work@gmail.com>
2023-10-08 15:54:59 +0530

    Expo Changes

--- App.tsx
+++ App.tsx
@@ -1,14 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useCallback } from 'react';
+import { Button, Linking, StyleSheet, View } from 'react-native';
+import useConnectToTwitter from './hooks/useConnectToTwitter';
 
 export default function App() {
+
+  const { initateTwitterAuth } = useConnectToTwitter();
+
+  
+
+  const handleTwitterAuth = useCallback(async () => {
+    const url = await initateTwitterAuth();
+    if (await Linking.canOpenURL(url)) {
+      await Linking.openURL(url);
+    } else {
+      console.log('Error in opening url');
+    }
+  }, [initateTwitterAuth]);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Button title='Connect to twitter' onPress={handleTwitterAuth} />
     </View>
   );
 }
+
 
 const styles = StyleSheet.create({
   container: {
@@ -16,5 +31,9 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    color: '#fff'
   },
+  text: {
+    color: '#000'
+  }
 });
